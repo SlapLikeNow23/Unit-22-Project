d00e700d5e78cea50ad5b70286c9b2ba2e2b83a5
Ruhit <13deyr@utcsheffield.org.uk>
2019-10-15 11:44:32 +0100

    changes

--- map.py
+++ map.py
@@ -2,74 +2,108 @@ import folium
 from folium import plugins
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import seaborn as sns
+import numpy as np
 import geopandas as gpd
+from shapely.geometry import Point
+from shapely.geometry import Polygon
 import csv
 import json
 import requests
+from folium.plugins import TimeSliderChoropleth
+from geopandas import GeoSeries, GeoDataFrame
 
 #%matplotlib inline
 
-df = pd.read_csv('/home/ruhit/Downloads/UTCs.csv')
-df.head()
+dfUTC = pd.read_csv('/home/ruhit/Downloads/UTCs.csv')
+print("UTCs\n", dfUTC.head())
 
 #Create map objefolium join datact
 m = folium.Map(location=[53.381130, -1.470085], zoom_start=5)
 
-for index, row in df.iterrows():
+for index, row in dfUTC.iterrows():
     #print(row)
     folium.Marker([row['Latitude'], row['Longitude']],
                   popup=row['University Technical College'],
                   icon=folium.Icon(icon='cloud')
                  ).add_to(m)
 
-'''
-shapefile = '/home/ruhit/Downloads/Local_Authority_Districts_April_2019_Boundaries_UK_BUC/Local_Authority_Districts_April_2019_Boundaries_UK_BUC.shp'
-
-#Read shapefile using Geopandas
-gdf = gpd.read_file(shapefile)[['LAD19NM', 'LAD19CD', 'st_area(shape)']]
-
-#Rename columns.
-gdf.columns = ['country', 'country_code', 'geometry']
-gdf.head()
-'''
-
-#region_file = 'http:///home/ruhit/Downloads/Local_Authority_Districts_April_2019_Boundaries_UK_BUC.geojson'
+region_file = '/home/ruhit/Downloads/Local_Authority_Districts_April_2019_Boundaries_UK_BUC.geojson'
 #regions = pd.read_csv()
 #print("regions", regions.head())
 
-
+'''
 url = 'https://opendata.arcgis.com/datasets'
 regions = f'{url}/bbb0e58b0be64cc1a1460aa69e33678f_0.geojson'
+'''
 
-geo_json_data = json.loads(requests.get(regions).text)
+fields=[str(y) for y in range(1997, 2017)]
+print(fields)
 
-folium.LayerControl().add_to(m)
+dfProsperity = pd.read_csv('/home/ruhit/Desktop/Unit 22 Map/Unit-22-Project/Prosperity1.csv')
+
+'''
+for y in range(1997, 2017):
+    folium.Choropleth(
+     geo_data = region_file,
+     name=str(y),
+     data=dfProsperity,
+     columns=['LAU1 code', str(y)],
+     key_on='feature.properties.lad19cd',
+     fill_color='YlGn',
+     fill_opacity=0.7,
+     line_opacity=0.2,
+     show=False,
+     overlay=True,
+    ).add_to(m)
+'''
+
+dfTime = dfProsperity.melt(id_vars=['LAU1 code','LA name'], var_name='year', value_name='prosperity', value_vars=fields)
+print("prosperity\n", dfTime.head())
 
 
-df = pd.read_csv('/home/ruhit/Desktop/Unit 22 Map/Unit-22-Project/Prosperity1.csv')
-print("prosperity", df.head())
+folium.Choropleth(
+     geo_data = region_file,
+     name='prosperity',
+     data=dfTime,
+     columns=['LAU1 code', 'prosperity'],
+     key_on='feature.properties.lad19cd',
+     fill_color='YlGn',
+     fill_opacity=0.7,
+     line_opacity=0.2,
+     #show=False,
+     #overlay=True,
+    ).add_to(m)
 
-p_data = pd.read_csv('/home/ruhit/Downloads/UTCs.csv')
 
-print("UTCs", p_data.head())
+'''
+for key in m._children:
+    if key.startswith('color_map'):
+        del(m._children[key])
+'''
+'''
+styledict = {
+    str(record): data.to_dict(orient='index') for
+    record, data in styledata.items()
+}
 
 
+g = TimeSliderChoropleth(
+    co2_geo_data_table_TS_subset.to_json(),
+    styledict=styledict, overlay = True
+).add_to(m)
 
-m.choropleth(
- geo_data = regions,
- name='choropleth',
- data=p_data,
- columns=['LAU1 code', '1997'],
- key_on='feature.lad19cd',
- fill_color='YlGn',
- fill_opacity=0.7,
- line_opacity=0.2,
-)
+'''
+
 folium.LayerControl().add_to(m)
 
 
 #in Local_Authority_Districts_April_2019_Boundaries_UK_BUC, join field name
+
+
+
+
 
 
 #Generate Map
